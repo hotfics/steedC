f28da7472737677d2c9b3e177266e583af21f911
hotfics <hotfics@foxmail.com>
2019-02-22 06:15:51 +0800

    add func

--- steedC/steedClite/steedCliteP/bitboards.cpp
+++ steedC/steedClite/steedCliteP/bitboards.cpp
@@ -1,4 +1,14 @@
 #include "bitboards.h"
+#include <assert.h>
 int popcount(Bitboard b) {
     return __builtin_popcountll(b);
+}
+int getlsb(Bitboard b) {
+    assert(b);  // lsb(0) is undefined
+    return __builtin_ctzll(b);
+}
+
+int getmsb(Bitboard b) {
+    assert(b);  // msb(0) is undefined
+    return __builtin_clzll(b) ^ 63;
 }
--- steedC/steedClite/steedCliteP/bitboards.h
+++ steedC/steedClite/steedCliteP/bitboards.h
@@ -1,2 +1,4 @@
 #include"types.h"
 int popcount(Bitboard b) ;
+int getlsb(Bitboard b) ;
+int getmsb(Bitboard b) ;
